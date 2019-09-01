9b2f505fb50c13164a83b86e9bdbb6738d21d473
lyhue1991 <lyhue1991@163.com>
2019-09-01 22:51:37 +0800

    add java

---
+++ ScannerDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+ 
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // 从键盘接收数据
+ 
+        // next方式接收字符串
+        System.out.println("next方式接收：");
+        // 判断是否还有输入
+        if (scan.hasNext()) {
+            String str1 = scan.next();
+            System.out.println("输入的数据为：" + str1);
+        }
+        scan.close();
+        System.out.println("程序结束");
+    }
+}
---
+++ test.scala
@@ -0,0 +1,5 @@
+Object test{
+    def main(args:Array[String]):Unit={
+        println("hello world")
+ }
+}
